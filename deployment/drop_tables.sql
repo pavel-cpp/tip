914df6fe081efea208c5238fc1da0610d4888475
pavel-cpp <toruchan174@gmail.com>
2024-02-17 02:04:30 +0300

    Added sql drop tables scripts

---
+++ deployment/drop_tables.sql
@@ -0,0 +1,7 @@
+drop table if exists main.font_settings cascade;
+
+drop table if exists main.image cascade;
+
+drop table if exists main.advanced_settings_passwords cascade;
+
+drop table if exists main.clients cascade;
