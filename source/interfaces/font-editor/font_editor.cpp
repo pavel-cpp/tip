233b477d22cd9583c4fd280551a0ce2f8d029211
pavel-cpp <toruchan174@gmail.com>
2024-02-20 02:25:48 +0300

    License applied

--- source/interfaces/font-editor/font_editor.cpp
+++ source/interfaces/font-editor/font_editor.cpp
@@ -1,3 +1,22 @@
+/*
+    "Text Insertion Program (TIP)" is a software
+    for client management and generating unique images for each client.
+    Copyright (C) 2024  Pavel Remdenok
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "font_editor.h"
 #include <ui_font_editor.h>
 
